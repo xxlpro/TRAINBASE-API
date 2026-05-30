5a729be7a0e14a47f3f81ad1e6c28745ac3b4747
xxlpro <olhowickov.danil.82@gmail.com>
2026-05-30 15:28:05 +0300

    Add name convertation (delete old version's alembic)

--- app/core/config.py
+++ app/core/config.py
@@ -19,6 +19,14 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
     )
 
+    naming_convention: dict[str, str] = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+    }
+
     @property
     def database_url(self) -> str:
         return (
--- app/db/base.py
+++ app/db/base.py
@@ -1,5 +1,13 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import get_settings
 
 
 class Base(DeclarativeBase):
-    pass
+    
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=get_settings().naming_convention
+    )
